d0f27037f526c814e170594825fd49eada773e2c
nadun <weerakkody.kn@gmail.com>
2024-12-09 15:44:47 +0530

    finish faqs

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -5,21 +5,76 @@ import NavigationBar from "../NavigationBar";
 const FAQ = () => {
   const faqs = [
     {
-      question: "What is the purpose of this project?",
+      question: "What is this project?",
       answer:
-        "The purpose of this project is to showcase a partnership engagement platform.",
+        "This is a militarized training camp filled with adventure and joy. Our aim is to provide you the needed team skills, survival skills, leadership and networking opportunities. We’ve added a little touch of Assassin’s Creed to make this even exciting. Don’t hesitate, apply now and enjoy later.",
+    },
+    {
+      question: "Who is doing this project?",
+      answer:
+        "This is proudly organised by the Professional Development Avenue of the Rotaract Club of SLIIT.",
+    },
+    {
+      question: "Can anyone take part?",
+      answer: "Absolutely!!! ANYONE between the ages 18 - 30 can take part.",
+    },
+    {
+      question: "Is it only for Rotractors?",
+      answer: "NO!!! This is literally for ANYONE between the ages 18 - 30. ",
+    },
+    {
+      question: "Is it only for SLIIT students?",
+      answer: "NO!!! This is literally for ANYONE between the ages 18 - 30. ",
+    },
+    {
+      question: "How many days is it?",
+      answer:
+        "This fun will extend beyond just one day. Having an overnight stay this will be carried for TWO amazingly fun filled days. ",
+    },
+    {
+      question: "Where will it be held?",
+      answer:
+        "Let’s all Assemble at the CHE adventure park (location details will be provided). ",
+    },
+    {
+      question: "What is the approximate price for a head? ",
+      answer:
+        "All of this GREAT fun for only a small amount of around LKR 4,000 - LKR 5,000.",
+    },
+    {
+      question: "Where can I find more information?",
+      answer:
+        "The Entire brotherhood of delegates will be added to a WhatsApp group where all messages will be communicated. We will also have a Delegate Handbook ready and made just for YOU. ",
     },
     {
       question: "How can I contact the team?",
       answer:
-        "You can contact us via the email or phone number provided on the Contact Us page.",
+        "Feel free to contact any of us in the ‘Contact Us’ page of our Official Brotherhood Website.",
     },
     {
-      question: "Where can I find more information daa?",
+      question: "What is this scavenger hunt thing?",
       answer:
-        "Additional details are available in the technical specifications section.",
+        "Shhhhh!!! That’s a secret for now. We will reveal it on the day. But it WILL be fun, competitive and exciting. ",
     },
-    // Add the rest of your FAQs here...
+    {
+      question: "How can you ensure safety of this?",
+      answer:
+        "Our Driving Force, the Team Alpha Organising Committee has successfully figured out all the details in case of an emergency including the locations of the nearest hospitals and transportation facilities have been arranged. ",
+    },
+    {
+      question: "What should I bring?",
+      answer:
+        "Basically EVERYTHING you need for two camping days including a bit of extra cash for emergencies (will include a checklist in the delegate handbook).",
+    },
+    {
+      question: "Where can we sleep?",
+      answer:
+        "Comfortable and safe tents will be provided from the Venue itself to provide you with the BEST OF THE BEST camping experience like nothing you’ve ever had. ",
+    },
+    {
+      question: "How fun will it be?",
+      answer: "EXTRMELY F****** Fun my dear creed brother!!!",
+    },
   ];
 
   const [openIndex, setOpenIndex] = useState(null);
